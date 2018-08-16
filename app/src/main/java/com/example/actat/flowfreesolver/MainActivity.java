45f2a43ddf66abe12deead65e627e722ab66f1d7
Actat <akiotoshima@gmail.com>
2018-08-16 12:56:02 +0900

    ボタンを配置した

--- app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
+++ app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.actat.flowfreesolver;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((Button)findViewById(R.id.button_reset)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // clickされた時の処理
+                Log.v("RESET", "button_reset clicked");
+            }
+        });
+
+        ((Button)findViewById(R.id.button_solve)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // clickされた時の処理
+                Log.v("SOLVE", "button_solve clicked");
+            }
+        });
     }
 }
