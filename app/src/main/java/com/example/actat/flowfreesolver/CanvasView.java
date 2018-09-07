d60a34d65ceaef8a9f893956fdfb2cbe5d0bdcb2
Actat <akiotoshima@gmail.com>
2018-09-07 19:02:13 +0900

    outline of canvasview

---
+++ app/src/main/java/com/example/actat/flowfreesolver/CanvasView.java
@@ -0,0 +1,44 @@
+package com.example.actat.flowfreesolver;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CanvasView extends View {
+
+    private Paint paint;
+
+    public CanvasView(Context context) {
+        super(context);
+        init_CanvasView();
+    }
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init_CanvasView();
+    }
+
+    public CanvasView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init_CanvasView();
+    }
+
+    private void init_CanvasView() {
+        paint = new Paint();
+    }
+
+    @Override
+    protected void  onDraw(Canvas canvas) {
+        // 描画処理をここに書く
+        invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return true;
+    }
+}
