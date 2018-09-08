a650682a7376c5cdd863ad5749ee626f00cbad31
Actat <akiotoshima@gmail.com>
2018-09-08 11:02:56 +0900

    remove meaningless import

--- app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
+++ app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
@@ -1,20 +1,12 @@
 package com.example.actat.flowfreesolver;
 
-import android.graphics.Matrix;
-import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Handler;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import java.util.Arrays;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     int SIZE = 5;
