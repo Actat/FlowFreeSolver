96c0e6942b63d78ec1a20c845f3151a088f872e7
Actat <akiotoshima@gmail.com>
2019-01-22 22:44:22 +0900

    log

--- app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
+++ app/src/main/java/com/example/actat/flowfreesolver/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
